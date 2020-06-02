699727efcb171fa69b75988df58838483d02e925
DanielCabral <Daniel-cabral14@hotmail.com>
2020-06-02 19:33:05 -0300

    migrations finalizadas

--- server/src/database/migrations/02_create_point_items.ts
+++ server/src/database/migrations/02_create_point_items.ts
@@ -2,8 +2,14 @@ import Knex from 'knex';
 
 export async function up(knex: Knex){
     return knex.schema.createTable('point_items',table => {
-        table.increments('id').primary()
-
+        table.increments('id').primary();
+        table.string('image')
+            .notNullable()
+            .references('id')
+            .inTable('points');
+        table.string('image').notNullable();
+        table.string('title').notNullable();
+        table.string('title').notNullable();
     });
 }
 
