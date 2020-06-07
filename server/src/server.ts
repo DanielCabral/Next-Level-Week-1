c40af3e777028fb4cb91f09a2f55f86f344913f4
DanielCabral <Daniel-cabral14@hotmail.com>
2020-06-07 12:34:35 -0300

    Removido imagens de uploads

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
-
 import routes from './routes';
+import { errors } from 'celebrate'
 
 const app=express();
 
@@ -13,5 +13,7 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/upload',express.static(path.resolve(__dirname,'..','uploads')));
+
+app.use(errors());
 
 app.listen(3333);
